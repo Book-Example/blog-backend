686480df1dbc857d1ad59c84079458befc92d7b1
iinsue <hinsu93@naver.com>
2022-03-22 12:42:12 +0900

    21.3.1 | 서버띄우기(608p)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-const hello = "hello";
+const Koa = require("koa");
+
+const app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = "hello world";
+});
+
+app.listen(4000, () => {
+  console.log("✅Listening to port:4000");
+});
+0;
