8b3323fa965bdec25af7536fc9955def49edd0de
iinsue <hinsu93@naver.com>
2022-03-23 21:04:37 +0900

    22.10.4 페이지 기능 구현(677p)

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -55,8 +55,24 @@ export const write = async (ctx) => {
   GET /api/posts
 */
 export const list = async (ctx) => {
+  // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
+  // 값이 주어지지 않았으면 기본값은 1입니다.
+  const page = parseInt(ctx.query.page || "1", 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const posts = await Post.find().exec();
+    // 포스트를 역순으로 불러오기
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set("Last-Page", Math.ceil(postCount / 10));
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e);
