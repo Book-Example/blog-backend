aaaca6b4f50bbcec516981d2eeeb3f599dbdf45a
iinsue <hinsu93@naver.com>
2022-03-24 10:37:47 +0900

    23.5.5 포스트 수정 및 삭제 시 권한 확인하기(709p)

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -3,15 +3,14 @@ import * as postsCtrl from "./posts.ctrl";
 import checkLoggedIn from "../../lib/checkLoggedIn";
 
 const posts = new Router();
-
 posts.get("/", postsCtrl.list);
 posts.post("/", checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); //  /api/posts/:id
-posts.get("/", postsCtrl.read);
-posts.delete("/", checkLoggedIn, postsCtrl.remove);
-posts.patch("/", checkLoggedIn, postsCtrl.update);
+post.get("/", postsCtrl.read);
+post.delete("/", checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch("/", checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
-posts.use("/:id", postsCtrl.checkObjectId, post.routes());
+posts.use("/:id", postsCtrl.getPostById, post.routes());
 
 export default posts;
