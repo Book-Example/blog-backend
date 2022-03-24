c4621bfff8943a395c46a4081882a125bbb1b3ed
iinsue <hinsu93@naver.com>
2022-03-24 10:15:53 +0900

    23.5.3 | 로그인했을 때만 API를 사용할수 있게 하기(707p)

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from "koa-router";
 import * as postsCtrl from "./posts.ctrl";
+import checkLoggedIn from "../../lib/checkLoggedIn";
 
 const posts = new Router();
 
 posts.get("/", postsCtrl.list);
-posts.post("/", postsCtrl.write);
+posts.post("/", checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); //  /api/posts/:id
 posts.get("/", postsCtrl.read);
-posts.delete("/", postsCtrl.remove);
-posts.patch("/", postsCtrl.update);
+posts.delete("/", checkLoggedIn, postsCtrl.remove);
+posts.patch("/", checkLoggedIn, postsCtrl.update);
 
 posts.use("/:id", postsCtrl.checkObjectId, post.routes());
 
