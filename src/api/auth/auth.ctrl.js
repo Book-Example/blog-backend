e527db1dff0c5414e6d0d2bafbe34009294e3ae6
iinsue <hinsu93@naver.com>
2022-03-24 09:46:46 +0900

    23.4.4 | 토큰 재발급&로그아웃(702p)

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -100,4 +100,11 @@ export const check = async (ctx) => {
   }
   ctx.body = user;
 };
-export const logout = async (ctx) => {};
+
+/*
+  POST  /api/auth/login
+*/
+export const logout = async (ctx) => {
+  ctx.cookies.set("access_token");
+  ctx.status = 204; // No content
+};
